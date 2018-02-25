032de903d08da208cfd32d27f0173d929c06c23b
Marcus Kainth <marcus@marcuskainth.co.uk>
2018-02-25 17:41:18 +0000

    msm8996: Remove ipacm
    * Disabled in O

--- msm8996.mk
+++ msm8996.mk
@@ -182,11 +182,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# IPACM
-PRODUCT_PACKAGES += \
-    IPACM_cfg.xml \
-    ipacm
-
 # IPv6
 PRODUCT_PACKAGES += \
     ebtables \
